fc87c4ff5d15cb8708b46d4fbe4193f913cb0864
Ulukai85 <ueffeldie7@gmx.de>
2025-05-08 20:06:51 +0200

    Do some Cleanup

--- streamlit-gui.py
+++ streamlit-gui.py
@@ -4,35 +4,50 @@ import os
 
 import visualization as vs
 
-PATHS = [
-    ["A", "B", "D", "H", "I", "J"],
-    ["A", "C", "F", "G", "I"],
-    ["B", "E", "H"]
-]
+def start_streamlit():
+    st.set_page_config(page_title="Schnitzelgraph", layout="wide")
 
-st.set_page_config(page_title="Schnitzelgraph", layout="wide")
+    st.title("Visualisierung von Pfaden durch einen Graphen")
 
-st.title("Visualisierung von Pfaden durch einen Graphen")
+    network, paths = get_example_graph_and_paths()
+    
+    handle_graph_and_paths(network, paths)
 
-graph = vs.get_directed_weighted_graph(vs.EDGES)
-network = vs.get_pyvis_network(graph)
-vs.style_pyvis_network(network)
-vs.visualize_network(network, path_to_highlight=[], html_name="base_graph.html")
 
-html_path = os.path.join(vs.FOLDER, "base_graph.html")
-with open(html_path, "r", encoding="utf-8") as f:
-    html_data = f.read()
+def handle_graph_and_paths(network, paths):
+    vs.style_pyvis_network(network)
 
-components.html(html_data, height=600, scrolling=True)
+    if st.button("Graph ohne Pfad"):
+        generate_button_and_graph(network, [])
 
-st.header("Mögliche Pfade")
+    for idx, path in enumerate(paths):
+        if st.button(f"Highlight Pfad {idx+1}: {' ➔ '.join(path)}"):
+            generate_button_and_graph(network, path)
 
-for idx, path in enumerate(PATHS):
-    if st.button(f"Highlight Pfad {idx+1}: {' ➔ '.join(path)}"):
-        vs.visualize_network(network, path_to_highlight=path, html_name="highlighted_graph.html")
-        
-        highlight_html_path = os.path.join(vs.FOLDER, "highlighted_graph.html")
-        with open(highlight_html_path, "r", encoding="utf-8") as f:
-            highlight_html = f.read()
+def get_example_graph_and_paths():
+    paths = [
+        ["A", "B", "D", "H", "I", "J"],
+        ["A", "C", "F", "G", "I"],
+        ["B", "E", "H"]
+    ]
+    graph = vs.get_directed_weighted_graph(vs.EDGES)
+    network = vs.get_pyvis_network(graph)
+    return network, paths
 
-        components.html(highlight_html, height=600, scrolling=True)
+
+def generate_button_and_graph(network, path):
+    vs.visualize_network(network, path_to_highlight=path, html_name="highlighted_graph.html")
+            
+    highlight_html_path = os.path.join(vs.FOLDER, "highlighted_graph.html")
+    with open(highlight_html_path, "r", encoding="utf-8") as f:
+        highlight_html = f.read()
+
+    components.html(highlight_html, height=600, scrolling=True)
+
+
+def main():
+    start_streamlit()
+
+
+if __name__ == "__main__":
+    main()
